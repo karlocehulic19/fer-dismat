fd5e5bfa95d80170c875ba10798a3f6d17d4fc00
Karlo Cehulic <karlocehlic@gmail.com>
2025-10-21 16:06:26 +0200

    lab1: create failing test

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,8 +1,29 @@
+class Potencija:
+    def __init__(self, potencija, koeficjent=1):
+        self.potencija = potencija
+        self.koeficjent = 1
+
+    def __mul__(self, other):
+        return Potencija(self.potencija + other.potencija, self.koeficjent * other.koeficjent)
+
+
+def lab1(a, b, c, d, e, L):
+    return 11340
+
+
 def main():
     duljine_slvoa = input("")
     a, b, c, d, e = duljine_slvoa.split(" ")
     L = input("")
+    rez = lab1(a, b, c, d, e, L)
+    print(rez)
+
+
+def test():
+    assert lab1(2, 2, 2, 2, 2, 10) == 11340
+    assert lab1(2, 2, 2, 2, 2, 1) == 5
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    test()
